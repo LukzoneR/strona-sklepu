6ef7b22b6b6d88b642bc6cebf859ca8fb5c8a3d8
gmrzyk <mrzykg@gmail.com>
2024-10-27 13:54:41 +0100

    Add files via upload

---
+++ Shop/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>sklep</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <div id="topbar">
+            <div>
+                <h3><img src="./icons/users.png" alt=""> Tysiące zadowolonych klientów</h3>
+            </div>
+
+            <div>
+                <h3><img src="./icons/truck.png" alt=""> Darmowa wysyłka w ten sam dzień</h3>
+            </div>
+
+            <div>
+                <h3><img src="./icons/calendar.png" alt=""> Od 10 lat na rynku</h3>
+            </div>
+        </div>
+        <div id="midbar">
+            <div id="logo" onclick="window.location.href='./index.php'">
+                <img src="" alt="Logo strony">
+            </div>
+
+            <div id="searchbar">
+                <form action="/search" method="GET">
+                    <input type="text" name="query" placeholder="Szukaj produktów...">
+                    <a href="/#">
+                        <img src="./icons/search.png" alt="Szukaj">
+                    </a>
+                </form>       
+            </div>
+
+            <div id="cart">
+                <a href="/#">
+                    <img src="./icons/cart.png" alt="Koszyk">
+                </a>
+            </div>
+        </div>
+        <div id="categories">
+
+        </div>
+    </header>
+    <main>
+        
+    </main>
+    <footer>
+        <div id="contact">
+
+        </div>
+    </footer>
+</body>
+</html>
