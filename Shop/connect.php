1586d818430515c5806c9578273a42584b8bf35f
Marcin Puzoń <marcinpzon@gmail.com>
2024-10-31 20:06:24 +0100

    stworzenie bazy danych i połączenie z projektem

---
+++ Shop/connect.php
@@ -0,0 +1,18 @@
+<?php
+
+$host = 'localhost'; 
+$user = 'root'; 
+$password = '';
+$database = 'sklep';
+
+$conn = new mysqli($host, $user, $password, $database);
+
+if ($conn->connect_error) {
+    die("Błąd połączenia: " . $conn->connect_error);
+}
+
+echo "Połączenie z bazą danych zostało nawiązane pomyślnie!";
+
+$conn->close();
+
+?>
